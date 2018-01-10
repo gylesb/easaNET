b590c2e858a42a324d5dd11f8e0c3e125bcb5b09
Gyles Batara <gyles.batara@live.com>
2018-01-10 10:31:12 -0800

    Protptype

---
+++ easaNET/Controllers/CrewController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using easaNET.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace easaNET.Controllers
+{
+	public class CrewController : Controller
+	{
+		// GET: /<controller>/
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(Crew crew)
+		{
+
+			return RedirectToAction("Result", crew);
+		}
+
+		public IActionResult Result(Crew crew)
+		{
+			return View(crew);
+		}
+	}
+}
---
+++ easaNET/Models/Crew.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace easaNET.Models
+{
+	public class Crew
+	{
+		public int Tasks { get; set; }
+		public int Hours { get; set; }
+
+		public Crew(int tasks, int hours)
+		{
+			Tasks = tasks;
+			Hours = hours;
+
+		}
+
+		public Crew()
+		{
+		}
+
+
+		public double GetAstronauts()
+		{
+			double astronauts = (Convert.ToDouble(Tasks) / Convert.ToDouble(Hours)) / 4;
+
+			return Math.Ceiling(astronauts);
+		}
+
+		public int GetHours()
+		{
+			return Hours;
+		}
+
+		public int GetTasks()
+		{
+			return Tasks;
+		}
+	}
+
+
+
+
+}
